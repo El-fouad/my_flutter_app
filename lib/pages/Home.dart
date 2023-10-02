8c03324d83df122056f10976a7877ab941bfe6bb
fouad <agri40@agri40.com>
2023-10-02 15:57:29 +0100

    add search bar in progress

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -8,53 +8,78 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "BreakFast",
-          style: TextStyle(
-              color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
+      appBar: appBar(),
+      body: Column(
+        children: [
+        Container(
+          margin: EdgeInsets.only(top: 40  ,  left: 20 , right: 20),
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.11),
+                blurRadius: 40,
+                spreadRadius: 0.0
+              )
+            ]
+          ),
+          child: TextField(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white
+            ),
+          ),
+        )
+      ]),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: const Text(
+        "BreakFast",
+        style: TextStyle(
+            color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
+      ),
+      centerTitle: true,
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+      elevation: 0.0,
+      leading: GestureDetector(
+        onTap: () => {},
+        child: Container(
+          margin: const EdgeInsets.all(10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 241, 223, 223),
+              borderRadius: BorderRadius.circular(10)),
+          child: const Text(
+            "<",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                color: Color.fromARGB(255, 0, 0, 0),
+                fontSize: 20,
+                fontWeight: FontWeight.w900),
+          ),
         ),
-        centerTitle: true,
-        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-        elevation: 0.0,
-        leading: GestureDetector(
+      ),
+      actions: [
+        GestureDetector(
           onTap: () => {},
           child: Container(
             margin: const EdgeInsets.all(10),
+            width: 30,
             alignment: Alignment.center,
             decoration: BoxDecoration(
                 color: const Color.fromARGB(255, 241, 223, 223),
                 borderRadius: BorderRadius.circular(10)),
-            child: const Text(
-              "<",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  color: Color.fromARGB(255, 0, 0, 0),
-                  fontSize: 20,
-                  fontWeight: FontWeight.w900),
-            ),
+            child: const Text("==",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    fontSize: 20,
+                    fontWeight: FontWeight.w900)),
           ),
         ),
-        actions: [
-          GestureDetector(
-            onTap: () => {},
-            child: Container(
-              margin: const EdgeInsets.all(10),
-              width: 30,
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 241, 223, 223),
-                  borderRadius: BorderRadius.circular(10)),
-              child: const Text("==",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Color.fromARGB(255, 0, 0, 0),
-                      fontSize: 20,
-                      fontWeight: FontWeight.w900)),
-            ),
-          ),
-        ],
-      ),
+      ],
     );
   }
 }
