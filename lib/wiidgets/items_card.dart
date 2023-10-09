6f33523be51b1e2924762f952b8e840855036748
fouad <agri40@agri40.com>
2023-10-09 17:33:36 +0100

    items cards wdget

---
+++ lib/wiidgets/items_card.dart
@@ -0,0 +1,35 @@
+
+import 'package:flutter/material.dart';
+import 'package:my_app/model/Products.dart';
+
+import '../constants/contants.dart';
+
+class ItemCard extends StatelessWidget {
+  final Product product;
+  final VoidCallback  press;
+  const ItemCard({
+    super.key,
+    required this.product,
+    required this.press,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: press,
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Expanded(
+          child: Container(
+            padding: const EdgeInsets.all(KDefaultPadding),
+            decoration: BoxDecoration(
+                color: product.color,
+                borderRadius: BorderRadius.circular(KDefaultPadding)),
+            child: Image.asset(product.image),
+          ),
+        ),
+        Text(product.title),
+        Text("\$ ${product.price}")
+      ]),
+    );
+  }
+}
