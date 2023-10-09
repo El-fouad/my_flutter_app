38e9ff4d04b8961571669c72b8aa8cfb4791359f
fouad <agri40@agri40.com>
2023-10-09 17:59:37 +0100

    details page

--- lib/pages/details/details_screen.dart
+++ lib/pages/details/details_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/constants/contants.dart';
 import 'package:my_app/model/Products.dart';
+import 'package:my_app/pages/details/components/body.dart';
 
 class DetailsSreen extends StatelessWidget {
-
   final Product product;
 
   const DetailsSreen({super.key, required this.product});
@@ -11,15 +12,26 @@ class DetailsSreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: product.color,
-      appBar: AppBar(
-        backgroundColor: product.color,
-        elevation: 0,
-        leading: IconButton(
-          onPressed: () =>Navigator.pop(context),
-          icon: const Icon(Icons.arrow_back), 
-          
-        ),
+      appBar: buildAppBar(context),
+      body: Body(product: product,),
+    );
+  }
+
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: product.color,
+      elevation: 0,
+      leading: IconButton(
+        onPressed: () => Navigator.pop(context),
+        icon: const Icon(Icons.arrow_back),
       ),
+      actions: [
+        IconButton(onPressed: () => {}, icon: const Icon(Icons.search)),
+        IconButton(onPressed: () => {}, icon: const Icon(Icons.add_shopping_cart)),
+        const SizedBox(
+          width: KDefaultPadding / 2,
+        )
+      ],
     );
   }
 }
