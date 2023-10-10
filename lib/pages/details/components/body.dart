47bd2cc14bcde03f5978bb02f255e8eb6c0a8ed4
fouad <agri40@agri40.com>
2023-10-10 15:08:45 +0100

    details page

--- lib/pages/details/components/body.dart
+++ lib/pages/details/components/body.dart
@@ -1,6 +1,9 @@
-
 import 'package:flutter/material.dart';
+import 'package:my_app/constants/contants.dart';
 import 'package:my_app/model/Products.dart';
+import 'package:my_app/pages/details/components/colorAndSize.dart';
+import 'package:my_app/pages/details/components/description.dart';
+import 'package:my_app/pages/details/components/producTitleWithImg.dart';
 
 class Body extends StatelessWidget {
   final Product product;
@@ -9,21 +12,56 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return  SingleChildScrollView(
-      child: Column(
-        children: [
-          SizedBox(
-            height: size.height,
-            child: Stack(
-              children: [
-                Container(
-                  margin: EdgeInsets.only(top: size.height * 0.3),
-                  height: 500 ,
-                  decoration: const BoxDecoration(color: Colors.white),
-                )
-              ],
-            ),
-          )
+    return SingleChildScrollView(
+      child: Column(children: [
+        SizedBox(
+          height: size.height,
+          child: Stack(
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: size.height * 0.32),
+                //  height: 500,
+                padding: EdgeInsets.only(
+                    top: size.height * 0.12,
+                    left: KDefaultPadding,
+                    right: KDefaultPadding),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30)),
+                ),
+                child: Column(children: [
+                  colorAndSize(product: product),
+                  Description(product: product),
+                  Row(
+                    children: [
+                      Row(
+                        children: [
+                          SizedBox(
+                            height: 32,
+                            width: 40,
+                            child: OutlinedButton(
+                              style: OutlinedButton.styleFrom(
+                                padding: EdgeInsets.zero,
+                                shape:const RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.all(Radius.circular(13))
+                                ) ,
+                              ),
+                              onPressed: () {}, 
+                              child: const Icon(Icons.remove),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  )
+                ]),
+              ),
+              productTitleWithImage(product: product),
+            ],
+          ),
+        )
       ]),
     );
   }
